8edfcbb6642dbfa27073731c8cff70d17972b038
Ramanathan Rm <rm.ramanathan@gmail.com>
2019-09-20 13:09:03 +0530

    Update tfrecords.py

--- utilities/tfrecords.py
+++ utilities/tfrecords.py
@@ -10,6 +10,51 @@ def _float_feature(value):
 def _bytes_feature(value):
   return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
 
+
+@timer
+# @checkTFversion
+def convert_to_tfrecords_float(filename, data_set, label):
+  """Converts a dataset to tfrecords."""
+  n=len(data_set)
+  if data_set.shape[0] != n:
+    raise ValueError('Images size %d does not match label size %d.' %(data_set.shape[0], n))
+  rows = data_set.shape[1]
+  cols = data_set.shape[2]
+  depth = data_set.shape[3]
+
+  # print('Writing', 'temp')
+  with tf.io.TFRecordWriter('temp.tfrecords') as writer:
+    for index in range(n):
+      image_raw = data_set[index].tostring()
+      l=int(label[index])
+      example = tf.train.Example(
+          features=tf.train.Features(
+              feature={'height': _float_feature(rows),'width': _float_feature(cols),'depth': _float_feature(depth),'label': _int64_feature(l),'image': _bytes_feature(image_raw)}
+          )
+      )
+      writer.write(example.SerializeToString())
+  print(filename)
+  copyfile('temp.tfrecords', filename)
+  os.remove('temp.tfrecords')
+
+@timer
+# @checkTFversion
+def parser_float(record):
+    keys_to_features = {
+        "image": tf.io.FixedLenFeature([], tf.string),
+        "label": tf.io.FixedLenFeature([], tf.int64),
+        "height": tf.io.FixedLenFeature([], tf.float16),
+        "width": tf.io.FixedLenFeature([], tf.float16),
+        "depth": tf.io.FixedLenFeature([], tf.float16)
+    }
+    parsed = tf.io.parse_single_example(record, keys_to_features)
+    parsed_image = tf.io.decode_raw(parsed["image"], tf.uint8)
+    parsed_image = tf.cast(parsed_image, tf.float32)
+    parsed_image=  tf.reshape(parsed_image, [ parsed["height"], parsed["width"], parsed["depth"]])    
+    label = tf.cast(parsed["label"], tf.int32)
+    return {'image': parsed_image}, label
+  
+  
 @timer
 # @checkTFversion
 def convert_to_tfrecords(filename, data_set, label):
